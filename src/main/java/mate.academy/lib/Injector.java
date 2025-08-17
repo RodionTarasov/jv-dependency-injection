4f93971ffbea145084534a2188846c75d86b66ef
kozma <kozmakruckov@gmail.com>
2025-08-17 22:19:33 +0300

    added jv-dependency-injection solution 3

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -28,41 +27,35 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> type) {
-        Class<?> clazz = findImplementation(type);
-        checkOfComponent(clazz);
-        if (clazz.isInterface()) {
-            return instances.get(clazz);
-        }
-        Object instance = createNewInstance(clazz);
-        Field[] declaredFields = clazz.getDeclaredFields();
-        for (Field field : declaredFields) {
-            if (field.isAnnotationPresent(Inject.class)) {
-                Object fieldInstance = getInstance(field.getType());
-                field.setAccessible(true);
-                try {
-                    field.set(instance, fieldInstance);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
+        try {
+            Class<?> clazz = findImplementation(type);
+            checkOfComponent(clazz);
+            if (clazz.isInterface()) {
+                return instances.get(clazz);
+            }
+            Object instance = createNewInstance(clazz);
+            Field[] declaredFields = clazz.getDeclaredFields();
+            for (Field field : declaredFields) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Object fieldInstance = getInstance(field.getType());
+                    field.setAccessible(true);
+                    try {
+                        field.set(instance, fieldInstance);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
-        }
-        instances.put(clazz, instance);
-        return instance;
-    }
-
-    private Object createNewInstance(Class<?> clazz) {
-        if (instances.containsKey(clazz)) {
-            return instances.get(clazz);
-        }
-        try {
-            Constructor<?> constructor = clazz.getConstructor();
-            Object instance = constructor.newInstance();
             instances.put(clazz, instance);
             return instance;
-        } catch (NoSuchMethodException | InvocationTargetException
-                 | InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException("can't create a new instance of" + clazz.getName(), e);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Can't create instance of " + type.getName(), e);
         }
+    }
+
+    private Object createNewInstance(Class<?> clazz) throws ReflectiveOperationException {
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        return constructor.newInstance();
     }
 
     private Class<?> findImplementation(Class<?> type) {
@@ -76,13 +69,13 @@ public class Injector {
         return type;
     }
 
-    private void checkOfComponent(Class<?> interfaceClazz) {
-        if (!interfaceClazz.isAnnotationPresent(Component.class)) {
+    private void checkOfComponent(Class<?> clazz) throws ReflectiveOperationException {
+        if (!clazz.isAnnotationPresent(Component.class)) {
             try {
-                throw new ReflectiveOperationException("Class" + interfaceClazz.getName()
-                        + "is not marked with annotation @Component");
+                throw new ReflectiveOperationException("Class " + clazz.getName()
+                        + " is not marked with annotation @Component");
             } catch (ReflectiveOperationException e) {
-                throw new RuntimeException(e);
+                throw new ReflectiveOperationException(e);
             }
         }
     }
