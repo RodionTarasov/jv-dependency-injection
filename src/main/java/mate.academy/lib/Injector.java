e0049c4c446d7a3f9271341d047d32122deb0976
kozma <kozmakruckov@gmail.com>
2025-08-17 21:51:48 +0300

    added jv-dependency-injection solution 2

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,5 +1,6 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
@@ -13,22 +14,27 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
+    private final Map<Class<?>, Class<?>> interfaceImplementation = new HashMap<>();
+    private final Map<Class<?>, Object> instances = new HashMap<>();
+
+    private Injector() {
+        interfaceImplementation.put(FileReaderService.class, FileReaderServiceImpl.class);
+        interfaceImplementation.put(ProductParser.class, ProductParserImpl.class);
+        interfaceImplementation.put(ProductService.class, ProductServiceImpl.class);
+    }
 
     public static Injector getInjector() {
         return injector;
     }
 
-    public Object getInstance(Class<?> interfaceClazz) {
-        Class<?> clazz = findImplementation(interfaceClazz);
+    public Object getInstance(Class<?> type) {
+        Class<?> clazz = findImplementation(type);
         checkOfComponent(clazz);
-        Field[] declaredFields = clazz.getDeclaredFields();
-        Object instance = null;
-        try {
-            instance = clazz.getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException
-                 | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
+        if (clazz.isInterface()) {
+            return instances.get(clazz);
         }
+        Object instance = createNewInstance(clazz);
+        Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 Object fieldInstance = getInstance(field.getType());
@@ -40,24 +46,44 @@ public class Injector {
                 }
             }
         }
+        instances.put(clazz, instance);
         return instance;
     }
 
-    private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementation = new HashMap<>();
-        interfaceImplementation.put(FileReaderService.class, FileReaderServiceImpl.class);
-        interfaceImplementation.put(ProductParser.class, ProductParserImpl.class);
-        interfaceImplementation.put(ProductService.class, ProductServiceImpl.class);
-        if (interfaceClazz.isInterface()) {
-            return interfaceImplementation.get(interfaceClazz);
+    private Object createNewInstance(Class<?> clazz) {
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
         }
-        return interfaceClazz;
+        try {
+            Constructor<?> constructor = clazz.getConstructor();
+            Object instance = constructor.newInstance();
+            instances.put(clazz, instance);
+            return instance;
+        } catch (NoSuchMethodException | InvocationTargetException
+                 | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("can't create a new instance of" + clazz.getName(), e);
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> type) {
+        if (type.isInterface()) {
+            Class<?> impl = interfaceImplementation.get(type);
+            if (impl == null) {
+                throw new RuntimeException("No implementation found for " + type.getName());
+            }
+            return impl;
+        }
+        return type;
     }
 
     private void checkOfComponent(Class<?> interfaceClazz) {
         if (!interfaceClazz.isAnnotationPresent(Component.class)) {
-            throw new RuntimeException("Class" + interfaceClazz.getName()
-                    + "is not marked with annotation @Component");
+            try {
+                throw new ReflectiveOperationException("Class" + interfaceClazz.getName()
+                        + "is not marked with annotation @Component");
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
